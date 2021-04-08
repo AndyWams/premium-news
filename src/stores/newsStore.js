4ab2cfd0409e349e0f42ed9382cc35666d277ba7
wams <andrew@lightbulbux.com>
2021-04-08 10:22:33 +0100

    update

--- src/stores/newsStore.js
+++ src/stores/newsStore.js
@@ -1,7 +1,7 @@
 import { makeAutoObservable } from "mobx";
 import { GetStoryIds } from "../service";
 import { DB_CONFIG } from "../config/config";
-import firebase from "firebase";
+import firebase from "firebase/app";
 import "firebase/database";
 class Store {
   storyIds = [];
