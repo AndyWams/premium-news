d9ee1f8eaf30134635237b4faf883030576ad48f
wams <andrew@lightbulbux.com>
2021-04-08 23:15:32 +0100

    update

--- src/components/comments.js
+++ src/components/comments.js
@@ -1,13 +1,13 @@
 import { observer } from "mobx-react-lite";
 import store from "../stores/newsStore";
 const Comments = () => {
-  //   const reversedList = [...store.comments];
+  const reversedList = [...store.comments];
   return (
     <div className="comments">
       <h4 className="mb-4">Comments</h4>
 
       {store.comments && store.comments.length > 0 ? (
-        store.comments.map((comment) => (
+        reversedList.reverse().map((comment) => (
           <pre
             key={comment.id}
             className="d-flex justify-content-between align-items-center"
